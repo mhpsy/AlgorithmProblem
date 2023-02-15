fdd51ccdc5b8cdff38cde39d2a96091061c92430
luu <luu@luu.luu>
2023-02-15 18:50:43 +0800

    简单看了看洛谷的东西,第三道题根本不想让我做

---
+++ luogu/src/top/mhpsy/y2023/m2/D15P1001.java
@@ -0,0 +1,12 @@
+package top.mhpsy.y2023.m2;
+
+import java.util.Scanner;
+
+public class D15P1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(n + m);
+    }
+}
---
+++ luogu/src/top/mhpsy/y2023/m2/D15P1002.java
@@ -0,0 +1,7 @@
+package top.mhpsy.y2023.m2;
+
+public class D15P1002 {
+    public static void main(String[] args) {
+
+    }
+}
